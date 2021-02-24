dfa67077a34981a0aee3f6ad72ce9bfe7e904fd0
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-02-24 19:49:09 +0100

    Ahora si que predice cosas de verdad, pronto se hara rework a deep learning

--- piedra_papel_tijeras_ai.py
+++ piedra_papel_tijeras_ai.py
@@ -19,71 +19,21 @@ def XD():
     X = np.array(datos.drop([prediccion], 1))
     y = np.array(datos[prediccion])
     
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.01)
     
     linear = linear_model.LinearRegression()
     linear.fit(X_train, y_train)
     precision = linear.score(X_test, y_test)
     
-    predicciones = linear.predict(X_test)
-
-    for X in range(len(predicciones)):
-        prediccion1 = predicciones[X]
-        #print(prediccion1)
-        prediccion1 = float(prediccion1)
-        eleccion = str(X_test[X])
-        #print(eleccion)
-        eleccion1 = eleccion[1:-3]
-        #print(eleccion1)
-        eleccion2 = int(eleccion1)
-        #print(type(eleccion))
-        #print(type(prediccion1))
-        victoria = y_test[X]
-        if prediccion1 > 0.5 and eleccion2 == 1:
-            #print("El bot dice que si eliges piedra ganas")
-            puntuacion1 += 1
-            print(puntuacion1)
-
-        if prediccion1 > 0.5 and eleccion2 == 2:
-            #print("El bot dice que si eliges papel ganas")
-            puntuacion2 += 1
-            print(puntuacion2, "!")
-
-        if prediccion1 > 0.5 and eleccion2 == 3:
-            #print("El bot dice que si eliges tijeras ganas")
-            puntuacion3 += 1
-            print(puntuacion3, "!!")
-
-
-
-        if prediccion1 > 1.5 and eleccion2 == 1:
-            #print("El bot dice que si eliges piedra empatas")
-            puntuacion1 -= 1
-
-        if prediccion1 > 1.5 and eleccion2 == 2:
-            #print("El bot dice que si eliges papel empatas")
-            puntuacion2 -= 1
-
-        if prediccion1 > 1.5 and eleccion2 == 3:
-            #print("El bot dice que si eliges tijeras empatas")
-            puntuacion3 -= 1    
-
-    if puntuacion1 > puntuacion2 and puntuacion3:
-        #print("la piedra es la mejor opcion")
-        eleccion = "piedra"
-
-    if puntuacion2 > puntuacion1 and puntuacion3:
-        #print("El papel es la mejor opcion")
-        eleccion = "papel"
-
-    if puntuacion3 > puntuacion2 and puntuacion1:
-        #print("Las tijeras es la mejor opcion")
-        eleccion = "tijeras"
+    
+    print(type(X_train))
 
 
     elecciones = ["piedra", "papel", "tijeras"]
+    eleccion = 0
 
 
+    
     #eleccion = random.choice(elecciones)
     #eleccion2 = random.choice(elecciones)
     eleccion1 = 0
@@ -91,15 +41,28 @@ def XD():
     victoria = 1
 
     eleccion2 = input("Piedra papel tijeras? ")
+    
 
     if eleccion2 == "piedra":
         eleccion1 = 1
+        e1 = np.array([[1, 1]])
+        
+        eleccion = linear.predict(e1)
 
     if eleccion2 == "papel":
         eleccion1 = 2
+        e1 = np.array([[2, 1]])
+        
+        eleccion = linear.predict(e1)
 
     if eleccion2 == "tijeras":
         eleccion1 = 3
+        e1 = np.array([[3, 1]])
+        
+        eleccion = linear.predict(e1)
+        print(eleccion)
+        
+    eleccion = elecciones[round(int(eleccion))]
 
     if eleccion == "piedra":
         eleccion4 = 1
@@ -165,9 +128,10 @@ def XD():
         with open("datos.csv", "a", newline='') as archivo:
             writer = csv.writer(archivo)
             writer.writerow([eleccion1, 2, eleccion4])
+    
+    otra = input("otra?")
+    if otra == "si":
+        XD()
 
 
 XD()
-otra = input("otra?")
-if otra == "si":
-    XD()
